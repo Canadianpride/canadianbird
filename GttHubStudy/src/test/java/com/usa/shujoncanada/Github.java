5ece2887c8e46314b9e582f138cecf0d74ef8e0a
jonyw <jonyw@192.168.86.61>
2018-02-21 20:48:15 -0500

    Version12

---
+++ GttHubStudy/src/test/java/com/usa/shujoncanada/Github.java
@@ -0,0 +1,43 @@
+package com.usa.shujoncanada;
+
+public class Github {
+	
+	
+	public static void main(String[] args) {
+		
+		int salary =300000;
+		int bonus =1000;
+		int total=salary+bonus;
+		
+	
+		
+		System.out.println("My name is Shujon. "+"My yearly salary is "+total+".");
+		
+		
+	}
+
+	
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
